b7a37dcb7cadd884644883032be3a8617dc8540d
Alexander mena <alexismena-90@hotmail.com>
2024-10-22 00:05:20 -0700

    Uodated the JS on th server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const PORT = 3001;
 const path = require("path");
-
+const db = require("./db/db.json");
 const app = express();
-app.arguments(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.use(express.urlencoded({ extended: true }));
+//app.use("/api", api);
+app.use(express.static("public"));
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+app.get("/api/notes", (req, res) => res.json(db));
+app.get("/jss", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/js/index.js"))
+);
+app.get("/css", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/css/styles.css"))
+);
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
